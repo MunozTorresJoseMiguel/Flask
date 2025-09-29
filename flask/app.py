452d7a227d72b2e61173aa1f2629177f2abc9246
JoseMiguelMunozTorres <23308060610631@cetis61.edu.mx>
2025-09-29 09:03:48 -0500

    calculadora flask

--- flask/app.py
+++ flask/app.py
@@ -3,30 +3,44 @@ from flask import Flask
 #para describir rutas es con el @app.router("/")
 app = Flask (__name__)
 #Esto nos crea nuestro servider
+
+
 @app.route("/")
 def hola_mundo():
-    return "<h1> hola mundo desde Flask! <h1>"
-
-@app.route("/otra")# route son las rutas
-def hola_mundo1():
-    return "<h1> hola mundo desde Flask En otra ruta! <h1>"
-
-#name y main guardan el tipo de dato de la app y activa el modo de depuracion
-
+    return '''
+<h1> Bienvenidos ala calculadora de Python! <h1>
+<h3>para sumar  teclear o escribe en el navegador 127.0.0.1:5000/sumar/10/20</h3>
+<h3>para restar teclear o escribe en el navegador 127.0.0.1:5000/restar/10/20</h3>
+<h3>para obtener el valor maximo  teclear o escribe en el navegador 127.0.0.1:5000/maximo/10/20</h3>
+'''
+@app.route("/sumar/<int:num1>/<int:num2>")
+def sumar(valor_uno,valor_dos):
+    resultado = valor_uno + valor_dos
+    return f'''
+<h1> El resultado de la suma es: {resultado} <h1>
+'''
+@app.route("/restar/<int:num1>/<int:num2>")
+def restar(num1,num2):
+    resultado = num1 - num2
+    return f'''
+<h1> El resultado de la resta es: {resultado} <h1>
+'''
+@app.route("/maximo/<int:num1>/<int:num2>")
+def maximo(num1,num2):
+    if num1 > num2:
+        resultado = num1
+    else:
+        resultado = num2
+    return f'''
+<h1> El numero maximo es: {resultado} <h1>
+'''
 if __name__ == "__main__":
     app.run(debug=True)
-#para ejecutar es python app.py y crea un servidor web para ejecutar paginas 
 
-#aun falta aprender a como poner las plantillas 
 
-#Werkzeug maneja las solicitudes y respuestas web 
 
-#jinja2 crea plantillas Html Dinamicas 
 
-#Concepto basico Cliente(navegador) servidor(Programa)
-
-#flask trabaja por medio de rutas  
-
-# Las rutas son direciones de navergacion de url 
-
-#toda ruta lleva su funcionalidad y se da por medio de una funcion def funcion de ruta()
+#bien venidoa las calculadora de python
+#para sumar  teclear o escribe en el navegador 127.0.0.1:5000/sumar/10/20
+#para restar teclear o escribe en el navegador 127.0.0.1:5000/restar/10/20
+#para obtener el valor maximo  teclear o escribe en el navegador 127.0.0.1:5000/maximo/10/20
